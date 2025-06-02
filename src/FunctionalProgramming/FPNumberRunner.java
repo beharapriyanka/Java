4a9723c0666081e14d21606b3112536b06dc5e26
beharapriyanka <beharapriyanka15@gmail.com>
2025-06-02 20:23:47 +0530

    lambda expression

--- src/FunctionalProgramming/FPNumberRunner.java
+++ src/FunctionalProgramming/FPNumberRunner.java
@@ -8,13 +8,16 @@ public class FPNumberRunner {
         int result = sumListFP(numbers);
         System.out.println(result);
 
+        int result1 = sumListFPLambda(numbers);
+        System.out.println(result1);
+
 
     }
 
     public static Integer sumList(List<Integer> numbers) {
         int sum = 0;
         for (Integer number : numbers) {
-            sum += number;
+            sum += number; //This is called as Mutations
         }
         return sum;
     }
@@ -23,4 +26,15 @@ public class FPNumberRunner {
         return numbers.stream().filter(num -> num%2 == 1).
                 reduce(0, (num1, num2) -> num1+num2);
     }
+
+    public static Integer sumListFPLambda(List<Integer> numbers) {
+        return numbers.stream().filter(num -> num%2 == 1).
+                reduce(0, (num1, num2) -> {
+                    System.out.println(num1 + " " + num2);
+                    return num1+num2;});
+    }
+/*the above method is having all the statements, but with lambda expression
+we can have these statements written in a single statement.
+ */
+
 }
